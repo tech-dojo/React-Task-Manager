cc0e2987d10cd57bcf0b958c635bb759a2ae3d31
shamiul <shoq9876@gmail.com>
2016-03-28 16:45:42 +0600

    data passed as props to table

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,13 +1,59 @@
 import React from 'react'
 class Products extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    //  this.state.products = [];
+    this.state = {};
+    this.state.products = [
+      {
+        id: 1,
+        category: 'Sporting Goods',
+        price: '49.99',
+        qty: 12,
+        name: 'Football'
+      }, {
+        id: 2,
+        category: 'Sporting Goods',
+        price: '9.99',
+        qty: 15,
+        name: 'Baseball'
+      }, {
+        id: 3,
+        category: 'Sporting Goods',
+        price: '29.99',
+        qty: 14,
+        name: 'Basketball'
+      }, {
+        id: 4,
+        category: 'Electronics',
+        price: '99.99',
+        qty: 34,
+        name: 'iPod Touch'
+      }, {
+        id: 5,
+        category: 'Electronics',
+        price: '399.99',
+        qty: 12,
+        name: 'iPhone 5'
+      }, {
+        id: 6,
+        category: 'Electronics',
+        price: '199.99',
+        qty: 23,
+        name: 'Nexu 7'
+      }
+    ];
+
+  }
+
   render() {
 
     return (
       <div>
-     <h1>Products</h1>
         <SearchBar/>
-        <ProductTable/>
+        <ProductTable products={this.state.products}/>
       </div>
     );
 
@@ -30,10 +76,26 @@ class SearchBar extends React.Component {
 class ProductTable extends React.Component {
 
   render() {
+    var product = this.props.products.map(function(product) {
+      return (<ProductRow product={product} key={product.id}/>)
+    });
     return (
       <div>
-        <h2>The Product Table</h2>
-        <ProductRow/>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>price</th>
+              <th>quantity</th>
+              <th>category</th>
+            </tr>
+          </thead>
+          <tbody>
+          { product}
+
+          </tbody>
+
+        </table>
       </div>
     );
 
@@ -46,10 +108,28 @@ class ProductRow extends React.Component {
   render() {
 
     return (
-      <div>
-        <h3>Product Row</h3>
-        <EditableCell/>
-      </div>
+      <tr>
+        <EditableCell cellData={{
+                "type": "name",
+                value: this.props.product.name,
+                id: this.props.product.id
+              }}/>
+            <EditableCell  cellData={{
+                type: "price",
+                value: this.props.product.price,
+                id: this.props.product.id
+              }}/>
+            <EditableCell  cellData={{
+                type: "qty",
+                value: this.props.product.qty,
+                id: this.props.product.id
+              }}/>
+            <EditableCell  cellData={{
+                type: "category",
+                value: this.props.product.category,
+                id: this.props.product.id
+              }}/>
+      </tr>
     );
 
   }
@@ -57,12 +137,11 @@ class ProductRow extends React.Component {
 }
 class EditableCell extends React.Component {
 
-
   render() {
     return (
-    <div>
-      <h4>Editable Cell</h4>
-    </div>
+      <td>
+        <h6>{this.props.cellData.value}</h6>
+      </td>
     );
 
   }
