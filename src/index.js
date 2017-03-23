2906504163f76310833d5a642ebda0f059ec05f0
Muhibbur Rahman <muhib1268@gmail.com>
2017-03-23 14:47:11 +0600

    File Reorganize

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Model from './Model'
-import EmployeeList from './employeeList.js';
-import TaskList from './taskList.js';
+import EmployeeList from './component/employee/employeeList.js';
+import TaskList from './component/task/taskList.js';
 import { Link, browserHistory } from 'react-router';
 import { Router, Route, IndexRoute } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
