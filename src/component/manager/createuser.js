a834dfae9d036ba36d7d2b9590b0e8a153ebf3b8
shamiul <shoq9876@gmail.com>
2017-04-23 00:22:32 +0600

    bundle updated

--- src/component/manager/createuser.js
+++ src/component/manager/createuser.js
@@ -78,6 +78,7 @@ export default class CreateUser extends React.Component {
             password: this.state.password,
             user_type: this.state.user_type
         }).then(function(response) {
+          browserHistory.push('/userSignin')
             console.log(response);
         }).catch(function(error) {
             console.log(error);
